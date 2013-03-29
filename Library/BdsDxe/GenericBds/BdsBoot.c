aa54d89c730f94c03aec814dda3b7be5a1eee508
SunnyKi <Kisarin.A.V@gmail.com>
2013-03-29 22:13:38 +0600

    missing open bracket

--- Library/BdsDxe/GenericBds/BdsBoot.c
+++ Library/BdsDxe/GenericBds/BdsBoot.c
@@ -270,7 +270,7 @@ MacOS:
     ImageInfo->LoadOptions      = buffer;
   }
 
-  if ((AsciiStrStr(gSettings.BootArgs, "-v") == 0) ||
+  if (((AsciiStrStr(gSettings.BootArgs, "-v") == 0) ||
       (AsciiStrStr(gSettings.BootArgs, "-x") == 0)) &&
       (gST->ConOut != NULL)) {
     gST->ConOut = NULL; 
