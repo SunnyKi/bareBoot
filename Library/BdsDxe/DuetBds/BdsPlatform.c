54f7eadc1d096154ae4ef7615efe87c27eabc945
SunnyKi <Kisarin.A.V@gmail.com>
2013-04-14 15:22:17 +0600

    solved tochpad and spd detection problem for some laptop

--- Library/BdsDxe/DuetBds/BdsPlatform.c
+++ Library/BdsDxe/DuetBds/BdsPlatform.c
@@ -349,7 +349,9 @@ DisableUsbLegacySupport(
                   break;
                 }
               }
+#if 0
               SaveValue &= ~0x003F;
+#endif
               PciIo->Pci.Write (PciIo, EfiPciIoWidthUint32, ExtendCap + 0x4, 1, &SaveValue);
             }
           }
