1a3891dc6197f8a1e9277423b6d981d720c9c0a0
SunnyKi <Kisarin.A.V@gmail.com>
2013-04-15 13:25:46 +0600

    fix system-id injection

--- Library/BdsDxe/GenericBds/macosx/DataHubCpu.c
+++ Library/BdsDxe/GenericBds/macosx/DataHubCpu.c
@@ -261,7 +261,7 @@ SetupDataForOSX (
   } else {
     if (!EFI_ERROR (PlatformUuidStatus)) {
       Status =  LogDataHub(&gEfiMiscSubClassGuid, L"system-id", &gUuid, sizeof(EFI_GUID));
-      Status =  LogDataHub(&gEfiMiscSubClassGuid, L"system-id", &gSystemID, sizeof(EFI_GUID));
+      Status =  LogDataHub(&gEfiMiscSubClassGuid, L"system-id", &gUuid, sizeof(EFI_GUID));
     }
   }
   return Status;
