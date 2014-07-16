80e1aff187cc17308a69c30509770093014f6fc7
Nikolai Saoukh <Nikolai.Saoukh@gmail.com>
2014-07-16 11:36:23 +0400

    Fine tune cppcheck.sh

--- cppcheck.sh
+++ cppcheck.sh
@@ -4,6 +4,13 @@ EDK2HOME=../edk2; export EDK2HOME
 
 exec cppcheck \
 	--library=$EDK2HOME/cppcheck/edk2.cfg \
+	--inconclusive \
+	\
+	-D HOST_EFI \
+	-D VBOX \
+	-D FSTYPE=hfs \
+	-D FSW_DEBUG_LEVEL=3 \
+	\
 	-I DxeIpl \
 	-I EfiLdr \
 	-I Include \
@@ -19,4 +26,9 @@ exec cppcheck \
 	-I $EDK2HOME/IntelFrameworkPkg/Include \
 	-I $EDK2HOME/IntelFrameworkModulePkg/Include \
 	-I $EDK2HOME/UefiCpuPkg/Include \
+	\
+	-i Library/PListLib/main.c \
+	-i Library/PListLib/plist_helpers_os.c \
+	-i VBoxFsDxe/msvc/ \
+	-i VBoxFsDxe/posix/ \
 	$*
