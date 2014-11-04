086f68974246c80e1f0e56aeee1f65efbc07b73e
Nikolai Saoukh <nikolai.saoukh@gmail.com>
2014-11-04 21:18:59 +0300

    OhciDxe: more readable ED & TD debug dumps

--- Bus/Pci/OhciDxe/OhciDebug.c
+++ Bus/Pci/OhciDxe/OhciDebug.c
@@ -57,13 +57,13 @@ OhciDumpEdTdInfo (
   if (Stage) {
     DEBUG ((EFI_D_INFO, "\n Before executing command\n"));
   } else {
-    DEBUG ((EFI_D_INFO, "\n after executing command\n"));
+    DEBUG ((EFI_D_INFO, "\n After executing command\n"));
   }
   if (Ed != NULL) {
-    DEBUG ((EFI_D_INFO, "\nED Address:%x, ED buffer:\n", (UINT32)(UINTN) Ed));
+    DEBUG ((EFI_D_INFO, "\nED Address: 0x%08x, ED buffer:\n", (UINT32)(UINTN) Ed));
     DEBUG ((EFI_D_INFO, "DWord0  :TD Tail :TD Head :Next ED\n"));
     for (Index = 0; Index < sizeof (ED_DESCRIPTOR) / 4; Index++) {
-      DEBUG ((EFI_D_INFO, "%8x ", *((UINT32*) (Ed) + Index)));
+      DEBUG ((EFI_D_INFO, "%08x ", *((UINT32*) (Ed) + Index)));
     }
     DEBUG ((EFI_D_INFO, "\nNext TD buffer:\n", (UINT32)(UINTN) Td));
   }
@@ -77,14 +77,14 @@ OhciDumpEdTdInfo (
     }else if (Td->Word0.DirPID == TD_NODATA_PID) {
       DEBUG ((EFI_D_INFO, "\nNo data PID "));
     }
-    DEBUG ((EFI_D_INFO, "TD Address:%x, TD buffer:\n", (UINT32)(UINTN) Td));
+    DEBUG ((EFI_D_INFO, "TD Address: 0x%08x, TD buffer:\n", (UINT32)(UINTN) Td));
     DEBUG ((EFI_D_INFO, "DWord0  :CuBuffer:Next TD :Buff End:Next TD :DataBuff:ActLength\n"));
     for (Index = 0; Index < sizeof (TD_DESCRIPTOR) / 4; Index++) {
-      DEBUG ((EFI_D_INFO, "%8x ", *((UINT32*) (Td) + Index)));
+      DEBUG ((EFI_D_INFO, "%08x ", *((UINT32*) (Td) + Index)));
     }
-    DEBUG ((EFI_D_INFO, "\nCurrent TD Data buffer(size%d)\n", (UINT32) Td->ActualSendLength));
+    DEBUG ((EFI_D_INFO, "\nCurrent TD Data buffer (size %d)\n", (UINT32) Td->ActualSendLength));
     for (Index = 0; Index < Td->ActualSendLength; Index++) {
-      DEBUG ((EFI_D_INFO, "%2x ", *(UINT8 *)(UINTN) (Td->DataBuffer+Index)));
+      DEBUG ((EFI_D_INFO, "%02x ", *(UINT8 *)(UINTN) (Td->DataBuffer+Index)));
     }
   Td = (TD_DESCRIPTOR *)(UINTN) (Td->NextTDPointer);
   }
