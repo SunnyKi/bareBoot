b21c67319ce943637667f136714489d639b46e13
Nikolai Saoukh <nikolai.saoukh@gmail.com>
2017-12-09 16:07:37 +0300

    Dead code off the workbench!
    Can be recovered with "git revert ..." however

--- BootSector/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-/Users/sunny/src/opt/local/cross/bin/i686-clover-linux-gnu-gcc -c -o bin/St32_64m.o St32_64m.S
-/Users/sunny/src/opt/local/cross/bin/i686-clover-linux-gnu-ld --oformat binary -o bin/St32_64m.com bin/St32_64m.o -Ttext 0 -Map bin/St32_64m.map
-rm bin/St32_64m.o bin/St32_64m.map
