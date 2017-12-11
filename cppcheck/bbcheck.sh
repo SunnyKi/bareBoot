172de4d61f39f7a8b458ef51b244cd5d0fdef5e3
Nikolai Saoukh <nikolai.saoukh@gmail.com>
2017-12-11 11:40:07 +0300

    cppcheck: adjust to current state

--- cppcheck/bbcheck.sh
+++ cppcheck/bbcheck.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+#	-D HOST_EFI \
+#	-D VBOX \
+#	-D FSTYPE=hfs \
+#	-D FSW_DEBUG_LEVEL=3 \
+#	\
+#	-i $BBHOME/Library/PListLib/main.c \
+#	-i $BBHOME/Library/PListLib/plist_helpers_os.c \
+#	-i $BBHOME/VBoxFsDxe/msvc/ \
+#	-i $BBHOME/VBoxFsDxe/posix/ \
+
+
 EDK2HOME=$BBHOME/../edk2; export EDK2HOME
 EDK2CFG=$BBHOME/cppcheck/edk2.cfg; export EDK2CFG
 
@@ -7,30 +18,23 @@ exec $BBHOME/cppcheck/cppcheck.sh \
 	--library=$BBHOME/cppcheck/bareBoot.cfg \
 	--inconclusive \
 	\
-	-D HOST_EFI \
-	-D VBOX \
-	-D FSTYPE=hfs \
-	-D FSW_DEBUG_LEVEL=3 \
+	-D MDE_CPU_X64 \
 	\
 	-I $BBHOME/DxeIpl \
 	-I $BBHOME/EfiLdr \
 	-I $BBHOME/Include \
-	-I $BBHOME/Library/BdsDxe \
-	-I $BBHOME/Library/BdsDxe/Graphics \
-	-I $BBHOME/Library/BdsDxe/GenericBds \
+	-I $BBHOME/BdsDxe \
+	-I $BBHOME/BdsDxe/Graphics \
+	-I $BBHOME/BdsDxe/GenericBds \
 	-I $BBHOME/Library/PListLib \
 	-I $BBHOME/Library/PListLib/b64 \
 	-I $BBHOME/VBoxFsDxe \
 	\
 	-I $EDK2HOME/MdePkg/Include \
-	-I $EDK2HOME/MdePkg/Include/Ia32 \
+	-I $EDK2HOME/MdePkg/Include/X64 \
 	-I $EDK2HOME/MdeModulePkg/Include \
 	-I $EDK2HOME/IntelFrameworkPkg/Include \
 	-I $EDK2HOME/IntelFrameworkModulePkg/Include \
 	-I $EDK2HOME/UefiCpuPkg/Include \
 	\
-	-i $BBHOME/Library/PListLib/main.c \
-	-i $BBHOME/Library/PListLib/plist_helpers_os.c \
-	-i $BBHOME/VBoxFsDxe/msvc/ \
-	-i $BBHOME/VBoxFsDxe/posix/ \
 	$*
